eeb8e57c1153d4bfa4b45d9b1b16ef30dad91c7d
Charonzinho <franciscoliveira04@gmail.com>
2022-11-09 00:17:51 +0000

    09/11 - 00:17 Chico

---
+++ script.js
@@ -0,0 +1,13 @@
+/*$(window).scroll(function(){
+    console.log("certo");
+    $('#nav').toggleClass('scrolled', $(this).scrollTop() > 10);
+});*/
+
+/*var nav = document.getElementById('#nav');
+
+window.onscroll = function (event) {
+    var scroll = window.pageXOffset;
+    if(scroll < 10){
+        nav.style.backgroundColor = rgba(141, 137, 137, 0.253);
+    }
+}*/
